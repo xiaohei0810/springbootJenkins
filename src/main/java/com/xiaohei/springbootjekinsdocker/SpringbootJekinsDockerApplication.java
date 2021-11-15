ae4f4a2719b9e889fa72390f7fcf4bcb3d1dff7f
liangyusheng <1127113014@qq.com>
2021-11-15 10:13:16 +0800

    springboot整合jenkins测试

---
+++ src/main/java/com/xiaohei/springbootjekinsdocker/SpringbootJekinsDockerApplication.java
@@ -0,0 +1,13 @@
+package com.xiaohei.springbootjekinsdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJekinsDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJekinsDockerApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/xiaohei/springbootjekinsdocker/controller/EntranceController.java
@@ -0,0 +1,22 @@
+package com.xiaohei.springbootjekinsdocker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author liangyusheng@xiaomi.com
+ * @Date 2021/11/15 10:07 上午
+ * @Version 1.0
+ * @Describtion
+ */
+@RestController
+@RequestMapping("/entrance")
+public class EntranceController {
+
+    @GetMapping("/test")
+    public String entranceTest(){
+        return "hello word";
+    }
+
+}
