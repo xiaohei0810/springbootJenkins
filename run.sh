347d40fd757d715cda5e86d71abe25b89a3684df
liangyusheng <1127113014@qq.com>
2021-11-15 16:04:23 +0800

    修改容器端口号映射

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@ docker ps -a
 
 docker stop webdemo-8452
 docker rm -f webdemo-8452
-docker run -d --name webdemo-8452  -p 8452:8452 xiaohei555/webdemo:1.0
+docker run -d --name webdemo-8452  -p 8081:8452 xiaohei555/webdemo:1.0
 
 docker ps -a
--- src/test/java/com/xiaohei/springbootjekinsdocker/SpringbootJekinsDockerApplicationTests.java
+++ src/test/java/com/xiaohei/springbootjekinsdocker/SpringbootJekinsDockerApplicationTests.java
@@ -8,6 +8,25 @@ class SpringbootJekinsDockerApplicationTests {
 
     @Test
     void contextLoads() {
+        test("1");
+    }
+
+
+    void test(String actionName) {
+        switch (actionName) {
+            //确认收货
+            case "1":
+                //晒单审核通过
+            case "2":
+                //取消申请退款
+            case "3":
+                //退款
+            case "4":
+                System.out.println("444444");
+                break;
+            default:
+                System.out.println("default");
+        }
     }
 
 }
